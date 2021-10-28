be89ae27c25f5f7b5c59862cfcc1efbf9d7e226f
Jan Dryml <honza.dryml@icloud.com>
2021-10-28 20:37:03 +0200

    add React router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import './App.css';
-
+import {Route, Switch} from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
 
+const HatsPage = (props) => {
+    console.log(props);
+    return (
+        <div>
+            <h1>Hats page</h1>
+        </div>
+    );
+}
+
 function App() {
-  return (
-    <div>
-        <HomePage/>
-    </div>
-  );
+    return (
+        <div>
+            <Switch>
+                <Route exact path='/' component={HomePage}/>
+                <Route path='/shop/hats' component={HatsPage}/>
+            </Switch>
+        </div>
+    );
 }
 
 export default App;
